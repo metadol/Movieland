d97acb4589ff950d223e4b03a13cf161ca97128a
kevin <kevingitabr@gmail.com>
2024-02-05 16:43:50 +0530

    fixed mixed content

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import searchicon from './search.svg'
 import MovieCard from './MovieCard';
 
 // 14fd1cf
-const API_URL = 'http://www.omdbapi.com/?apikey=14fd1cf';
+const API_URL = 'https://www.omdbapi.com/?apikey=14fd1cf';
 
 
 const movie1 = {
